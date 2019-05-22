abab2f33573bcc38760ac650210651f47c630a62
Nick Major <nickmajor78@gmail.com>
2019-05-22 18:11:15 -0400

    page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkComponent } from './work/work.component';
+import { EmploymentComponent } from './employment/employment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/summary', pathMatch: 'full' },
+    { path: 'summary', component: SummaryComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'previous-work', component: WorkComponent },
+    { path: 'previous-employment', component: EmploymentComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkComponent } from './work/work.component';
+import { EmploymentComponent } from './employment/employment.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ClarityModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      SidenavComponent,
+      SummaryComponent,
+      ResumeComponent,
+      WorkComponent,
+      EmploymentComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      ClarityModule,
+      RouterModule,
+      BrowserAnimationsModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
