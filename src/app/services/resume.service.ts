d82277101319d61449966d89e39d968b34c68b9a
Nick Major <nickmajor78@gmail.com>
2019-05-28 21:38:36 -0400

    header spacing

--- src/app/services/resume.service.ts
+++ src/app/services/resume.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import ResumeJSON from './resume.json';
 import { User } from './interfaces.js';
+import EmploymentJSON from './employment.json';
+import ProjectJSON from './projects.json';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ResumeService {
     private user: User = ResumeJSON;
+    private employment = EmploymentJSON;
+    private projects = ProjectJSON;
 
     constructor() { }
 
@@ -14,4 +18,11 @@ export class ResumeService {
         return this.user;
     }
 
+    getEmployment() {
+        return this.employment;
+    }
+
+    getProjects() {
+        return this.projects;
+    }
 }
