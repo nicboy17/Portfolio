931e5b10ca31243e78bfb429d2418ef3dd0e9a78
Nick Major <nickmajor78@gmail.com>
2019-05-28 21:39:03 -0400

    employment css

--- src/app/employment/employment.component.ts
+++ src/app/employment/employment.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { ResumeService } from '../services/resume.service';
 
 @Component({
-  selector: 'app-employment',
-  templateUrl: './employment.component.html',
-  styleUrls: ['./employment.component.scss']
+    selector: 'app-employment',
+    templateUrl: './employment.component.html',
+    styleUrls: ['./employment.component.scss']
 })
 export class EmploymentComponent implements OnInit {
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
+    employment = [];
 
-  constructor() { }
+    constructor(private resume: ResumeService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+        this.employment = this.resume.getEmployment();
+
+        this.galleryOptions = [
+            {
+                breakpoint: 2000,
+                image: false,
+                thumbnailsRemainingCount: true,
+                height: '100px',
+                width: '100%',
+                thumbnailsColumns: 5,
+                imageAnimation: NgxGalleryAnimation.Slide
+            },
+            // max-width 400
+            {
+                breakpoint: 400,
+                preview: false
+            }
+        ];
+    }
 
 }
